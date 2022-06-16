184ef64b41450ba92eaa5efcef451ec2ba68cd32
rajeshdobhal28 <rajesh.dobhal28@gmail.com>
2022-06-16 23:58:23 +0530

    added reame

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('/health', function(req, res, next) {
     res.status(200).send('health check');
 });
 
+
+app.use(function(req, res) {
+    res.status(404).send('<h1>404 Not Found</h1>');
+});
+
 app.listen(8081, function() {
     console.log("Server started port 8081");
 });
